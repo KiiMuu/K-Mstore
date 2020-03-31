0da3a7b362ccd1358d0622d5b6fc4c682f146855
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-03-31 20:20:34 +0200

    define a product model with mysql sequelize

--- models/product.js
+++ models/product.js
@@ -4,6 +4,24 @@ const sequelize = require('../util/database');
 
 const Product = sequelize.define('product', {
     id: {
-        
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primarykey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 });
+
+module.exports = Product;
