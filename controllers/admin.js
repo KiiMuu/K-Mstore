4e3120369d7cf9857be7f3146492736fc2191843
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-04-05 21:38:57 +0200

    add async requests to delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -161,8 +161,8 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId).then(product => {
         if (!product) {
             return next(new Error('No product found!'));
@@ -170,10 +170,8 @@ exports.postDeleteProduct = (req, res, next) => {
         fileHelper.deleteFile(product.imageUrl);
         return Product.deleteOne({ _id: prodId, userId: req.user._id });
     }).then(() => {
-        res.redirect('/admin/products');
+        res.status(200).json({ message: 'Success!' }); 
     }).catch(err => {
-        const error =  new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
+        res.status(500).json({ message: 'Deleting failed!' });
     });
 }
